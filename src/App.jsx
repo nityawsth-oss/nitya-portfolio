e00f47f872854be53266f6751e31a03e6d361e62
nityawsth-oss <nityawsth@gmail.com>
2026-09-11 17:07:07 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -410,11 +410,11 @@ function App() {
             </span>
 
             <span className="block text-fuchsia-500">
-              NITYA
+              NITYA BHONDU
             </span>
 
             <span className="block">
-              AWASTHI
+              BADHI BHONDU pagal gadhi AWASTHI
             </span>
 
           </h1>
